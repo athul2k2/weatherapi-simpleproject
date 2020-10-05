ec8ef07a5deb55025afbc73c28ba0e74284f0ccc
athul krishna <athulkrishnapv6@gmail.com>
2020-10-05 14:56:00 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const https = require("https")
+const bodyParser = require("body-parser")
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",function(req , res ){
+
+  res.sendFile(__dirname + "/index.html")
+
+})
+
+app.post("/", function(req , res){
+
+   const query =req.body.cityName;
+   const apiKey ="07b7d4de819d3d48fe8ac412fd6a26c0"
+   const unit = "metric"
+   const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit;
+
+   https.get(url , function(response){
+     console.log(response.statusCode)
+
+     response.on("data", function(data){
+       const weatherData = JSON.parse(data)
+       const temp = weatherData.main.temp
+       const weatherDescription =weatherData.weather[0].description
+       const icon = weatherData.weather[0].icon
+       const imageURL = "https://openweathermap.org/img/wn/"+ icon +"@2x.png"
+       res.write("<p>The weather is currently <p> "+ weatherDescription );
+       res.write("<h1>The temperature in "+query+" is "+ temp +"degrees Celcius.</h1>")
+       res.write("<img src =" +imageURL+">")
+       res.send();
+
+       console.log(weatherDescription);
+     })
+   })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("server is running on port 3000")
+})
